6ce472324e9b74c3614c21c9d37ea1d83db0b107
Kunal-sharmaa <65971343+Kunal-sharmaa@users.noreply.github.com>
2022-09-17 14:26:47 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavbarComponent } from './shares/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule, NgbNavbar } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './shares/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FormControlComponent } from './shares/form-control/form-control.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    FormControlComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
